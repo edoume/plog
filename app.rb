25c849b2f807e1c3f82a68ee6ac8f96ba29faf38
edoume <iphone.courriel@gmail.com>
2014-11-06 15:52:09 +0100

    keep pushing

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
+require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
-DataMapper.setup(:default, 'sqlite:///app.db')
+# créer la base de donnée app.db
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/app.db")
 
-get '/about' do
-  erb :about
+class Log
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  property :body, Text
+end
+
+DataMapper.finalize
+
+Log.auto_upgrade!
+
+get '/' do
+  @logs = Log.all(:order => [ :id.desc ], :limit => 20)
+  erb :index
 end
