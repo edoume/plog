a5f9f51429f9bd9d83333dffc0b03dbbb9c99484
edoume <iphone.courriel@gmail.com>
2014-11-12 17:04:39 +0100

    mod bouton del edit

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require "better_errors"
 # créer la base de donnée app.db
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/app.db")
 
+#créer la table de la BDD
 class Log
   include DataMapper::Resource
   property :id, Serial
@@ -12,21 +13,27 @@ class Log
   property :body, Text
 end
 
+#finalise la table de la BDD
 DataMapper.finalize
 
+#mets à jour la BDD
 Log.auto_upgrade!
 
+#affiche les logs dans index
 get '/' do
   @logs = Log.all(:order => [ :id.desc ], :limit => 20)
   erb :index
 end
 
+#créé dans la BDD via la methode post
 post '/' do
 @post = Log.create(params[:log])
 redirect '/'
 end
 
 #Log.update(:body => "new text here")
+#Log.delete(:id??)
+
 
 helpers do
 	def color(name)
@@ -39,4 +46,3 @@ helpers do
 		end
 	end
 end
-
